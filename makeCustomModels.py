eb08cb250b35e3cd95febec7665dbe6e8deeb761
Elliot Plant <elliot@index.com>
2017-11-16 17:47:22 -0800

    cp

--- makeCustomModels.py
+++ makeCustomModels.py
@@ -2,9 +2,13 @@ import os
 import sys
 import fileinput
 import getpass
+from pathlib import Path
+
+home = str(Path.home())
 
 # A helper script to build models from local protos and use them in bamboo
-os.chdir('/Users/elliotplant/source/com.tappmo')
+os.chdir(home)
+os.chdir('source/com.tappmo')
 
 os.system('./scripts/build.sh')
 os.system('rm -rf ../bamboo/modules/ixmodel/app/model/com')
